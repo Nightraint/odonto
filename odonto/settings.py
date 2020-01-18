e55ec878c2278fdcc6a46cb99f06c75ca54f9ec2
emilianorua@gmail.com <emilianorua@gmail.com>
2020-01-17 22:33:25 -0300

    initial2

--- odonto/settings.py
+++ odonto/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v25!wz%4cudvv$xj#scz84oj7&_)1gdbc6_b*%k8(&7g_hddz&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','192.168.0.18','*','127.0.0.1']
 ALLOWED_HOSTS = ['emiodonto.herokuapp.com']
